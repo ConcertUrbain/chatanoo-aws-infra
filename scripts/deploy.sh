905d3649ceefad3ba75d3a97923dc4215e28bbfa
Mathieu Desvé <mathieu@ogury.co>
2016-10-18 17:58:09 +0200

    Improve deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,13 @@
 echo "Deploy $TRAVIS_TAG version to S3"
-aws s3 cp infra/network.cfn.yml s3://chatanoo-deployment/infra/network/$TRAVIS_TAG.cfn.yml
-aws s3 cp infra/main.cfn.yml s3://chatanoo-deployment/infra/main/$TRAVIS_TAG.cfn.yml
+aws s3 cp infra/network.cfn.yml s3://chatanoo-deployment.eu-west-1/infra/network/$TRAVIS_TAG.cfn.yml
+aws s3 cp infra/main.cfn.yml s3://chatanoo-deployment.eu-west-1/infra/main/$TRAVIS_TAG.cfn.yml
 
 echo "Upload latest"
 aws s3api put-object \
-  --bucket chatanoo-deployment \
+  --bucket chatanoo-deployment.eu-west-1 \
   --key infra/network/latest.cform \
   --website-redirect-location /infra/network/$TRAVIS_TAG.cfn.yml
 aws s3api put-object \
-  --bucket chatanoo-deployment \
+  --bucket chatanoo-deployment.eu-west-1 \
   --key infra/main/latest.cfn.yml \
   --website-redirect-location /infra/main/$TRAVIS_TAG.cfn.yml
